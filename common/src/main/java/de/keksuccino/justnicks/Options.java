ffa0e35136853233312769c41a7b69f6fd2a5fe8
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-12-08 13:47:27 +0100

    v1.0.0 - 21

--- common/src/main/java/de/keksuccino/justnicks/Options.java
+++ common/src/main/java/de/keksuccino/justnicks/Options.java
@@ -8,7 +8,7 @@ public class Options extends AbstractOptions {
     protected final Config config = new Config(JustNicks.MOD_DIR.getAbsolutePath().replace("\\", "/") + "/options.txt");
 
     public final Option<Boolean> showOriginalIdentityToSelfPlayer = new Option<>(config, "show_original_identity_to_self_player", true, "general");
-    public final Option<Boolean> refreshSelfOnNick = new Option<>(config, "refresh_self_player_on_nick", true, "general");
+    public final Option<Boolean> refreshSelfOnNick = new Option<>(config, "refresh_self_player_on_nick", false, "general");
 
     public Options() {
         this.config.syncConfig();
