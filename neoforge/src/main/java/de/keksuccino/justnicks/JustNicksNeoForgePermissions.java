97fb09fad1c2c7b321dcff36665f3d0b8af57d50
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-12-09 09:09:39 +0100

    v1.0.0 - 26

--- neoforge/src/main/java/de/keksuccino/justnicks/JustNicksNeoForgePermissions.java
+++ neoforge/src/main/java/de/keksuccino/justnicks/JustNicksNeoForgePermissions.java
@@ -10,14 +10,17 @@ import org.jetbrains.annotations.Nullable;
 public class JustNicksNeoForgePermissions {
 
     public static final PermissionNode<Boolean> NICK = new PermissionNode<>(Permission.NICK.getAsLocation(), PermissionTypes.BOOLEAN, (player, playerUUID, context) -> false);
+    public static final PermissionNode<Boolean> UNNICK = new PermissionNode<>(Permission.UNNICK.getAsLocation(), PermissionTypes.BOOLEAN, (player, playerUUID, context) -> false);
 
     public static void registerAll(PermissionGatherEvent.Nodes e) {
         e.addNodes(NICK);
+        e.addNodes(UNNICK);
     }
 
     @Nullable
     public static PermissionNode<Boolean> resolveNode(@NotNull Permission permission) {
         if (permission == Permission.NICK) return NICK;
+        if (permission == Permission.UNNICK) return UNNICK;
         return null;
     }
 
